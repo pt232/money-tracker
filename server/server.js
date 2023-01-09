5a149f751396ceb9e21b283a9c7108bb493a692c
Tobias Peters <peterstobias2302@gmail.com>
2023-01-09 17:36:51 +0100

    Add typescript support on the server

--- server/server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello World 👋");
-});
-
-app.listen(5000, () => console.log("Server listening on port 5000"));
---
+++ server/src/index.ts
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello World 👋");
+});
+
+app.listen(port, () =>
+  console.log(`⚡️ Server is running at http://localhost:${port}`)
+);
