8d675c5116bb4c8913fd5df06b40b8717ddb27b7
Tobias Peters <peterstobias2302@gmail.com>
2023-01-20 20:11:19 +0100

    feat(mongoose): add a connection to a mongodb database with mongoose

--- apps/api/src/env.d.ts
+++ apps/api/src/env.d.ts
@@ -2,6 +2,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       PORT: string;
+      MONGO_URL: string;
     }
   }
 }
--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,15 +1,19 @@
 import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+const mongoUrl = process.env.MONGO_URL;
 
 app.get("/", (req, res) => {
   res.send("Hello World 👋");
 });
 
-app.listen(port, () =>
-  console.log(`⚡️ [server] Server is running at http://localhost:${port}`),
-);
+mongoose.connect(mongoUrl).then(() => {
+  app.listen(port, () =>
+    console.log(`⚡️ Server is running at http://localhost:${port}`),
+  );
+});
