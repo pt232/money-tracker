2c73d6688a66528aa5a740bcee45eb551545b617
Tobias Peters <peterstobias2302@gmail.com>
2023-02-03 15:03:07 +0100

    fix(jest): separate express app from server file

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,21 +1,14 @@
 import dotenv from "dotenv";
-import express from "express";
 import mongoose from "mongoose";
+import app from "./app";
 
 dotenv.config();
 
-const app = express();
 const port = process.env.PORT || 5000;
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost:27017/";
-
-app.get("/", (req, res) => {
-  res.send("Hello World 👋");
-});
 
 mongoose.connect(mongoUrl).then(() => {
   app.listen(port, () =>
     console.log(`Server is running at http://localhost:${port}`),
   );
 });
-
-export default app;
