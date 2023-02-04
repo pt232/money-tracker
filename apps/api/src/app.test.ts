da41e50dbcce5b49368734af63abc63842221a7f
Tobias Peters <peterstobias2302@gmail.com>
2023-02-04 12:18:12 +0100

    refactor(test): remove should from test description to fit jest docs

--- apps/api/src/app.test.ts
+++ apps/api/src/app.test.ts
@@ -2,7 +2,7 @@ import request from "supertest";
 import app from "./app";
 
 describe("GET /", () => {
-  it("should return the welcome message", async () => {
+  it("returns the welcome message", async () => {
     const res = await request(app).get("/");
     expect(res.status).toBe(200);
     expect(res.text).toBe("Hello World 👋");
