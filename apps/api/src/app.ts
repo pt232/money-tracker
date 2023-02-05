fe6893c634ad173973d24d6969be80e71279a1c2
Tobias Peters <peterstobias2302@gmail.com>
2023-02-05 16:31:22 +0100

    feat(swagger): add swagger documentation for welcome route

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,6 +1,14 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+import yaml from "yamljs";
 
 const app = express();
+
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(yaml.load("./swagger.yml")),
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World 👋");
