c214755d7b26f34a592480f0a5b10ddc53673eda
Tobias Peters <peterstobias2302@gmail.com>
2023-02-07 11:03:16 +0100

    feat(login-page): add login header message

--- apps/web/src/pages/Login/Login.tsx
+++ apps/web/src/pages/Login/Login.tsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom";
 import { LoginForm } from "./LoginForm";
+import logo from "@/assets/logo-light.svg";
 
 export function Login() {
-  return <LoginForm />;
+  return (
+    <>
+      <div>
+        <img src={logo} alt="Logo" />
+        <h1>Log in with your account</h1>
+        <p>
+          Or <Link to="/">sign up now</Link> to track your expenses
+        </p>
+      </div>
+      <LoginForm />
+    </>
+  );
 }
