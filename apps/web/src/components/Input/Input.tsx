968ef9091475201c21060eaf1fdec34d0fd15458
Tobias Peters <peterstobias2302@gmail.com>
2023-02-07 17:47:58 +0100

    feat(login-page): add basic validation with zod

--- apps/web/src/components/Input/Input.tsx
+++ apps/web/src/components/Input/Input.tsx
@@ -1,18 +1,38 @@
+import React from "react";
 import styles from "./Input.module.css";
 
 type InputProps = {
   id: string;
   label: string;
+  type?: "text" | "email" | "password";
+  required?: boolean;
+  errorMessage?: string;
   infoMessage?: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-export function Input({ id, label, infoMessage }: InputProps) {
+export function Input({
+  id,
+  label,
+  errorMessage,
+  infoMessage,
+  type = "text",
+  required = false,
+  onChange,
+}: InputProps) {
   return (
     <div className={styles.input}>
       <label htmlFor={id} className={styles.inputLabel}>
         {label}
       </label>
-      <input type="text" id={id} className={styles.inputField} />
+      <input
+        type={type}
+        id={id}
+        required={required}
+        className={styles.inputField}
+        onChange={onChange}
+      />
+      {errorMessage && <p className={styles.errorMessage}>{errorMessage}</p>}
       {infoMessage && <p className={styles.infoMessage}>{infoMessage}</p>}
     </div>
   );
--- apps/web/src/pages/Login/LoginForm.tsx
+++ apps/web/src/pages/Login/LoginForm.tsx
@@ -1,14 +1,56 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { z } from "zod";
 import { Button } from "../../components/Button/Button";
 import { Card } from "../../components/Card/Card";
 import { Input } from "../../components/Input/Input";
 import styles from "./LoginForm.module.css";
 
+const FormData = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
 export function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    setErrorMessage("");
+
+    try {
+      FormData.parse({ email, password });
+      navigate("/");
+    } catch (err) {
+      if (err instanceof z.ZodError) {
+        setErrorMessage(err.issues[0].message);
+      }
+    }
+  }
+
   return (
     <Card>
-      <form className={styles.loginForm}>
-        <Input id="email" label="Email" />
-        <Input id="password" label="Password" infoMessage="Forgot Password?" />
+      <form className={styles.loginForm} onSubmit={handleSubmit}>
+        <Input
+          id="email"
+          label="Email"
+          type="email"
+          required
+          errorMessage={errorMessage}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          id="password"
+          label="Password"
+          type="password"
+          required
+          infoMessage="Forgot Password?"
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <Button label="Log in" type="submit" />
       </form>
     </Card>
