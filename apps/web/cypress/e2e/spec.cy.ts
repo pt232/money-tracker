520a1983fb8713f58b416520c50fb6b6e4f10f21
Tobias Peters <peterstobias2302@gmail.com>
2023-02-08 18:54:04 +0100

    test(login-page): add cypress test for the login form

--- apps/web/cypress/e2e/spec.cy.ts
+++ apps/web/cypress/e2e/spec.cy.ts
@@ -1,6 +1,12 @@
-describe("home page", () => {
-  it("renders the home page", () => {
-    cy.visit("/");
+describe("login page", () => {
+  it("submits the form and navigates to the homepage", () => {
+    cy.visit("/login");
+
+    cy.get("input[type='email']").type("test@example.com");
+    cy.get("input[type='password']").type("password");
+    cy.get("button[type='submit']").click();
+
+    cy.location("pathname").should("eq", "/");
   });
 });
 
