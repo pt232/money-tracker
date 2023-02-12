0798e5dc7f04196616aac40a3932685c6ac40d80
Tobias Peters <peterstobias2302@gmail.com>
2023-02-12 21:45:43 +0100

    test(login-form): fix default import

--- apps/web/src/pages/Login/Form/LoginForm.test.tsx
+++ apps/web/src/pages/Login/Form/LoginForm.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { MemoryRouter } from "react-router-dom";
-import { LoginForm } from "./LoginForm";
+import LoginForm from "./LoginForm";
 
 describe("LoginForm", () => {
   it("displays error messages when form data is invalid", async () => {
