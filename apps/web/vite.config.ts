28ebffe31d39a28713bb7c527161f10b6c5d20bf
Tobias Peters <peterstobias2302@gmail.com>
2023-02-13 17:49:50 +0100

    feat(dark-mode): add dark mode styles for login page

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import autoprefixer from "autoprefixer";
+import stringHash from "string-hash";
 
 export default defineConfig({
   resolve: {
@@ -15,6 +16,15 @@ export default defineConfig({
   css: {
     modules: {
       localsConvention: "camelCase",
+      generateScopedName(name, filename, css) {
+        if (name === "dark") return "dark";
+
+        const i = css.indexOf(`.${name}`);
+        const lineNumber = css.substring(0, i + 1).split(/[\r\n]/).length;
+        const hash = stringHash(css).toString(36).substring(0, 6);
+
+        return `_${name}_${hash}_${lineNumber}`;
+      },
     },
     postcss: {
       plugins: [autoprefixer()],
