d6bd9bafba411723869117f313cd813a5c11e5e0
Tobias Peters <peterstobias2302@gmail.com>
2023-02-13 11:26:30 +0100

    test(cypress): add a login command

--- apps/web/cypress/e2e/spec.cy.ts
+++ apps/web/cypress/e2e/spec.cy.ts
@@ -2,9 +2,7 @@ describe("login page", () => {
   it("submits the form and navigates to the homepage", () => {
     cy.visit("/login");
 
-    cy.get("input[type='email']").type("test@example.com");
-    cy.get("input[type='password']").type("password");
-    cy.get("button[type='submit']").click();
+    cy.login("test@example.com", "password");
 
     cy.location("pathname").should("eq", "/");
   });
--- apps/web/cypress/support/commands.ts
+++ apps/web/cypress/support/commands.ts
@@ -1,3 +1,9 @@
 /// <reference types="cypress" />
 
+Cypress.Commands.add("login", (email: string, password: string) => {
+  cy.get("input[type='email']").type(email);
+  cy.get("input[type='password']").type(password);
+  cy.get("button[type='submit']").click();
+});
+
 export {};
