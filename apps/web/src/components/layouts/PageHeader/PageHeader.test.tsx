ad7af406ca49b83525a88d4dbf8fdd73c1b058df
Tobias Peters <peterstobias2302@gmail.com>
2023-02-23 12:50:40 +0100

    test(page-header): test title rendering and button clicks

---
+++ apps/web/src/components/layouts/PageHeader/PageHeader.test.tsx
@@ -0,0 +1,45 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import PageHeader from "./PageHeader";
+
+describe("PageHeader", () => {
+  it("renders the title correctly", () => {
+    const title = "Hello World";
+    render(
+      <PageHeader
+        title={title}
+        setSidebarActive={() => {}}
+        toggleDarkMode={() => {}}
+      />,
+    );
+    expect(screen.getByText(title)).toBeInTheDocument();
+  });
+
+  it("handles button clicks", async () => {
+    const setSidebarActive = vi.fn();
+    const toggleDarkMode = vi.fn();
+    const user = userEvent.setup();
+
+    render(
+      <PageHeader
+        title=""
+        setSidebarActive={setSidebarActive}
+        toggleDarkMode={toggleDarkMode}
+      />,
+    );
+
+    await user.click(
+      screen.getByRole("button", {
+        name: "Open Menu",
+      }),
+    );
+    await user.click(
+      screen.getByRole("button", {
+        name: "Use Dark Mode",
+      }),
+    );
+
+    expect(setSidebarActive.mock.calls).toHaveLength(1);
+    expect(toggleDarkMode.mock.calls).toHaveLength(1);
+  });
+});
